baefdc5c6a9c15c1ba537aabd0896cfd5eb0999d
k29laksh <klaxay0505@gmail.com>
2024-09-25 14:57:04 +0530

    name change

--- backend_node/controllers/user.controllers.js
+++ backend_node/controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/AsyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const generateToken = async (userId) => {
   const user = await User.findById(userId);
